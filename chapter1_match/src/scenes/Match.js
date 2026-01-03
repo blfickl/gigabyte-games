2ed8a59747ec22e746434dfc4a8b8740b1a196c1
Bridget Fickling <bridget@meetbiddy.com>
2026-01-02 21:15:32 -0600

    Add chapter1_match

--- chapter1_match/src/scenes/Match.js
+++ chapter1_match/src/scenes/Match.js
@@ -43,42 +43,44 @@ class MatchScene extends Phaser.Scene {
     const shuffledBacks = Phaser.Utils.Array.Shuffle([...gaitData]);
 
 // In create()
-shuffledGaits.forEach((item, i) => {
-  // Create a container for border and text
-  const container = this.add.container(60, 100 + i * 70);
-  // Draw border rectangle
-  const border = this.add.graphics();
-  border.lineStyle(3, 0x222288, 1);
-  border.strokeRoundedRect(0, 0, 140, 44, 10);
-  // Add text
-  const gaitText = this.add.text(10, 5, item.gait, {
-    font: "24px Arial",
-    color: "#222",
-    padding: { x: 10, y: 8 },
-    backgroundColor: "rgba(0,0,0,0)"
-  })
-    .setInteractive({ draggable: true })
-    .setData("gait", item.gait);
-  container.add([border, gaitText]);
-  gaitText.container = container;
-  this.input.setDraggable(gaitText);
-  this.gaitTexts.push(gaitText);
-});
+    shuffledGaits.forEach((item, i) => {
+      // Create a container for border and text
+      const container = this.add.container(30, 40 + i * 50);
+      // Draw border rectangle
+      const border = this.add.graphics();
+      border.lineStyle(2, 0x222288, 1);
+      border.strokeRoundedRect(0, 0, 110, 32, 8);
+      // Add text
+      const gaitText = this.add.text(8, 2, item.gait, {
+        font: "16px Arial",
+        color: "#222",
+        padding: { x: 6, y: 4 },
+        backgroundColor: "rgba(0,0,0,0)"
+      })
+        .setInteractive({ draggable: true })
+        .setData("gait", item.gait);
+      container.add([border, gaitText]);
+      gaitText.container = container;
+      gaitText.gaitIndex = i;
+      this.input.setDraggable(gaitText);
+      this.gaitTexts.push(gaitText);
+    });
 
 
     // Display Back Function targets (droppables)
     shuffledBacks.forEach((item, i) => {
-      const backText = this.add.text(400, 60 + i * 100, item.back, {
-        font: "20px Arial",
+      const backText = this.add.text(180, 40 + i * 90, item.back, {
+        font: "13px Arial",
         backgroundColor: "#fffbe0",
         color: "#333",
-        wordWrap: { width: 320 },
-        padding: { x: 10, y: 8 },
-        borderRadius: 8
+        wordWrap: { width: 180 },
+        padding: { x: 6, y: 4 },
+        borderRadius: 6
       })
         .setData("gait", item.gait)
         .setData("awareness", item.awareness)
-        .setData("matched", false);
+        .setData("matched", false)
+        .setData("backIndex", i);
       this.backTargets.push(backText);
     });
 
@@ -107,37 +109,39 @@ this.input.on("dragend", (pointer, gameObject) => {
   if (gameObject.container) gameObject.container.setAlpha(1);
 });
 this.input.on("drop", (pointer, gameObject, dropTarget) => {
-  if (
-    dropTarget.getData("gait") === gameObject.getData("gait") &&
-    !dropTarget.getData("matched")
-  ) {
-    dropTarget.setData("matched", true);
-    if (gameObject.container) {
-      gameObject.container.x = dropTarget.x - 120;
-      gameObject.container.y = dropTarget.y + 10;
-      gameObject.disableInteractive();
-      gameObject.container.disableInteractive();
-    } else {
-      gameObject.x = dropTarget.x - 120;
-      gameObject.y = dropTarget.y + 10;
-      gameObject.disableInteractive();
-    }
-    this.showPopup(dropTarget.getData("awareness"));
-    this.matched++;
-    if (this.matched === gaitData.length) {
-      this.time.delayedCall(1200, () => this.showPopup("All matched! Well done!", true));
-    }
-  } else {
-    // Snap back if not matched
-    if (gameObject.container) {
-      gameObject.container.x = gameObject.container.input.dragStartX;
-      gameObject.container.y = gameObject.container.input.dragStartY;
-    } else {
-      gameObject.x = gameObject.input.dragStartX;
-      gameObject.y = gameObject.input.dragStartY;
-    }
-  }
-});
+      if (
+        dropTarget.getData("gait") === gameObject.getData("gait") &&
+        !dropTarget.getData("matched")
+      ) {
+        dropTarget.setData("matched", true);
+        if (gameObject.container) {
+          // Place the gait box a bit lower under the matched Back Function
+          const backIdx = dropTarget.getData("backIndex");
+          gameObject.container.x = 180;
+          gameObject.container.y = 40 + backIdx * 90 + 60;
+          gameObject.disableInteractive();
+          gameObject.container.disableInteractive();
+        } else {
+          gameObject.x = dropTarget.x - 120;
+          gameObject.y = dropTarget.y + 32;
+          gameObject.disableInteractive();
+        }
+        this.showPopup(dropTarget.getData("awareness"));
+        this.matched++;
+        if (this.matched === gaitData.length) {
+          this.time.delayedCall(1200, () => this.showPopup("All matched! Well done!", true));
+        }
+      } else {
+        // Snap back if not matched
+        if (gameObject.container) {
+          gameObject.container.x = gameObject.container.input.dragStartX;
+          gameObject.container.y = gameObject.container.input.dragStartY;
+        } else {
+          gameObject.x = gameObject.input.dragStartX;
+          gameObject.y = gameObject.input.dragStartY;
+        }
+      }
+    });
 
     // Enable drop zones
     this.backTargets.forEach(target => {
@@ -150,26 +154,26 @@ this.input.on("drop", (pointer, gameObject, dropTarget) => {
     if (this.popupIcon) this.popupIcon.destroy();
     // If final, show golden message at top with sparkles, not in popup
     if (isFinal) {
-      const congratsText = this.add.text(400, 40, "All matched! Well done!", {
-        font: "36px Arial",
+      const congratsText = this.add.text(30, 30, "All matched! Well done!", {
+        font: "18px Arial",
         color: "#ffd700",
         fontStyle: "bold",
-        align: "center",
+        align: "left",
         stroke: "#fff",
-        strokeThickness: 4
-      }).setOrigin(0.5).setDepth(20);
+        strokeThickness: 2
+      }).setOrigin(0, 0).setDepth(20);
       // Sparkles
-      for (let i = 0; i < 18; i++) {
-        const angle = (i / 18) * Math.PI * 2;
-        const x = 400 + Math.cos(angle) * 120;
-        const y = 40 + Math.sin(angle) * 32;
-        const sparkle = this.add.star(x, y, 5, 4, 10, 0xfff700).setDepth(21);
+      for (let i = 0; i < 10; i++) {
+        const angle = (i / 10) * Math.PI * 2;
+        const x = 110 + Math.cos(angle) * 30;
+        const y = 38 + Math.sin(angle) * 12;
+        const sparkle = this.add.star(x, y, 5, 3, 7, 0xfff700).setDepth(21);
         this.tweens.add({
           targets: sparkle,
           alpha: { from: 1, to: 0 },
           scale: { from: 1, to: 2 },
           duration: 900,
-          delay: i * 60,
+          delay: i * 40,
           onComplete: () => sparkle.destroy()
         });
       }
@@ -179,41 +183,19 @@ this.input.on("drop", (pointer, gameObject, dropTarget) => {
       });
       return;
     }
-    // Popup background
-    const popup = this.add.rectangle(400, 300, 420, 120, 0x222288, 0.95).setDepth(10);
+    // Popup background (smaller, more to left)
+    const popup = this.add.rectangle(270, 220, 260, 80, 0x222288, 0.95).setDepth(10);
     // Popup text
-    const popupText = this.add.text(400, 300, text, {
-      font: "22px Arial",
+    const popupText = this.add.text(270, 220, text, {
+      font: "16px Arial",
       color: "#fff",
       align: "center",
-      wordWrap: { width: 380 }
+      wordWrap: { width: 220 }
     })
       .setOrigin(0.5)
       .setDepth(11);
-    // Horseshoe icon (placeholder) at bottom left
-    const horseshoe = this.add.graphics({ x: 210, y: 355 });
-    horseshoe.lineStyle(6, 0xffd700, 1);
-    horseshoe.beginPath();
-    horseshoe.arc(0, 0, 24, Phaser.Math.DegToRad(45), Phaser.Math.DegToRad(315), false);
-    horseshoe.strokePath();
-    // Horseshoe ends
-    horseshoe.lineStyle(6, 0xffd700, 1);
-    horseshoe.beginPath();
-    horseshoe.moveTo(16, 16);
-    horseshoe.lineTo(24, 8);
-    horseshoe.moveTo(-16, 16);
-    horseshoe.lineTo(-24, 8);
-    horseshoe.strokePath();
-    horseshoe.setDepth(12);
-    // Animate horseshoe (rotate 3 times)
     this.tweens.add({
-      targets: horseshoe,
-      angle: 360 * 3,
-      duration: 1800,
-      repeat: 0
-    });
-    this.tweens.add({
-      targets: [popup, popupText, horseshoe],
+      targets: [popup, popupText],
       scale: { from: 0.7, to: 1 },
       alpha: { from: 0, to: 1 },
       duration: 350,
@@ -221,11 +203,9 @@ this.input.on("drop", (pointer, gameObject, dropTarget) => {
     });
     this.popup = popup;
     this.popupText = popupText;
-    this.popupIcon = horseshoe;
     this.input.once("pointerdown", () => {
       popup.destroy();
       popupText.destroy();
-      horseshoe.destroy();
     });
   }
 }
